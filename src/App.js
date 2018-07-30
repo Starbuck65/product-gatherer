383df5befa599f4e7f618d061598d289cf91a6fe
Francisco Javier Balderas Cejudo <frbal5@retch078-xw0004.ikea.com>
2018-07-30 13:20:22 +0200

    Working

--- src/App.js
+++ src/App.js
@@ -6,12 +6,8 @@ import { Query } from "react-apollo";
 import { ApolloProvider } from 'react-apollo';
 
 import * as ptags from './templates/ptags';
-import { Grid, Form, Button, Container, Header, Icon, Divider, Select } from 'semantic-ui-react';
-
-
-const client = new ApolloClient({
-    uri: "https://graphqlserver-productsinfo.herokuapp.com/"
-})
+import { Grid, Form, Button, Container, Header, Icon, Divider, Select, Checkbox } from 'semantic-ui-react';
+import {DateInput}  from 'semantic-ui-calendar-react';
 
 var DIM_OPTIONS = [
   { value: '50_120', text: '50x120cm'},
@@ -19,9 +15,112 @@ var DIM_OPTIONS = [
   { value: 'a5' , text: 'a5'}
 ];
 
+class PtagPrinter extends React.Component {
+  constructor (props) {
+        super(props);
+    this.state = {
+      product: props.product,
+      ptagSize: '25_60',
+      discount: 10,
+      date_start: '',
+      date_end: '',
+      discountPrice: 0,
+      manualPrice: false
+    }
+  }
+
+  handleChange = (e, {name, value}) => {
+    if (this.state.hasOwnProperty(name)) {
+      this.setState({ [name]: value });
+    }
+  }
+
+  handleCheck = (e, data) => {
+    this.setState({ [data.name]: data.checked });
+  }
+
+
+  printDocument = () => {
+    const input = document.getElementById('printarea');
+    var finalPrice = 0 ;
+    if (this.state.manualPrice){
+      finalPrice = this.state.discountPrice;
+    }else {
+      finalPrice = this.state.product.normalPrice - (this.state.product.normalPrice * (this.state.discount/100));
+      finalPrice = finalPrice.toFixed(2);
+    }
+    switch (this.state.ptagSize) {
+      case '50_120':
+        ptags.ptag.pb25x60(this.state.product,finalPrice,this.state.date_start, this.state.date_end);
+        break;
+     case '25_60':
+       ptags.ptag.pb25x60(this.state.product,finalPrice,this.state.date_start, this.state.date_end);
+       break;
+     case 'a5':
+       ptags.ptag.a5(this.state.product,finalPrice);
+       break;
+      default:
+    }
+  }
+
+  render() {
+    return(
+      <div>
+      <p><b>Name:</b>{this.state.product.name}</p>
+      <p><b>Type: </b>{this.state.product.type}</p>
+      <p><b>Normal price: </b>{this.state.product.normalPrice}</p>
+      <p><b>Discount Price: </b>{this.state.product.normalPrice - (this.state.product.normalPrice * (this.state.discount/100))}</p>
+
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Field>
+            <label>From:</label>
+            <DateInput
+            inline
+            name="date_start"
+            dateFormat='DD/MM/YY'
+            value={this.state.date_start}
+            onChange={this.handleChange}/>
+          </Form.Field>
+          <Form.Field>
+            <label>To:</label>
+            <DateInput
+            inline
+            name="date_end"
+            dateFormat='DD/MM/YY'
+            value={this.state.date_end}
+            onChange={this.handleChange}
+            />
+          </Form.Field>
+        </Form.Group>
+        <Form.Group widths='equal'>
+          <Form.Input type = "number" label="Discount" id="discount" name='discount' value={this.state.discount} onChange={this.handleChange}/>
+          <Form.Field>
+            <label>Manual Price</label>
+            <Checkbox toggle checked={this.state.manualPrice} name='manualPrice' onChange={this.handleCheck}/>
+          </Form.Field>
+          <Form.Input type = "number" label="Reduced price" id="discountPrice" name='discountPrice' disabled = {!this.state.manualPrice} value={this.state.discountPrice} onChange={this.handleChange}/>
+
+        </Form.Group>
+          <Form.Field control={Select} label='Size' name='ptagSize' options={DIM_OPTIONS} placeholder='Size' onChange={this.handleChange} />
+          <Button type='submit' onClick={this.printDocument}>Download PDF</Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+
+
+const client = new ApolloClient({
+    uri: "https://graphqlserver-productsinfo.herokuapp.com/"
+})
+
+
+
 const GET_INFO_PRODUCT = gql`
-query product($partNumber: String!) {
-  product(partNumber: $partNumber)  {
+query product($partNumber: String!, $lang: String!) {
+  product(partNumber: $partNumber, lang: $lang)  {
     partNumber
     name
     type
@@ -31,6 +130,7 @@ query product($partNumber: String!) {
     familyPrice_endDate
     familyPrice_price
     familyPrice_disclaimer
+    lang
   }
 }
 `
@@ -45,8 +145,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       partNumber: '',
-      selectedOption: '',
-      discount: 10
+      lang: 'de',
+      product: {}
     };
   };
 
@@ -60,57 +160,35 @@ class App extends React.Component {
     this.setState({
       partNumber: document.getElementById("input_value").value
     });
-    console.log(this.state)
+    //console.log(this.state)
   }
 
    submitSearch = (event) => {
      event.preventDefault()
-     //console.log(this.state)
+     ////console.log(this.state)
    }
 
-   handleChange = (e, {value}) => {
-     this.setState({ selectedOption: value });
-     //console.log('Option selected: ', value);
-   }
-
-
-
-   printDocument=(data) => {
-     const input = document.getElementById('printarea');
-     console.log(this.state.selectedOption);
-     var size = 'a4';
-     var orientation = 'p';
-     switch (this.state.selectedOption) {
-       case '50_120':
-         size = [50,120];
-         break;
-      case '25_60':
-        ptags.ptag.pb25x60(data.product,this.state.discount);
-        break;
-      case 'a5':
-        size = 'a4';
-        console.log('sdf');
-        orientation = 'l';
-        ptags.ptag.a5(data.product,this.state.discount);
-        break;
-       default:
-        size = 'a4';
-        orientation= 'p';
+   handleChange = (e, {name, value}) => {
+     if (this.state.hasOwnProperty(name)) {
+       this.setState({ [name]: value });
      }
+   }
+
+
+
 
 //     var doc = new jspdf(orientation,'cm',size);//this.state.selectedOption.value);
 //     doc.fromHTML(input);
 //     var nameFile = this.state.partNumber + '.pdf';
 //     doc.save(nameFile);
 
-   }
 
   render() {
 
   //  const { status } = this.state;
     //const { selectedOption } = this.state.selectedOption;
 
-console.log(ptags.ptag)
+
     return (
       <ApolloProvider
         client={client}>
@@ -125,64 +203,48 @@ console.log(ptags.ptag)
            <Divider />
         </Grid.Column></Grid.Row>
         <Grid.Row><Grid.Column>
-        <Grid columns={3} divided>
+        <Grid columns={1} divided>
         <Grid.Row>
           <Grid.Column>
           <Form>
-            <Form.Field control={Select} label='Language' options={languages} placeholder='Language' />
+            <Form.Group widths='equal'>
+            <Form.Field control={Select} label='Language' options={languages} name='lang' onChange={this.handleChange} value={this.state.lang} placeholder='Language' />
             <Form.Field>
               <label>Enter the product number:</label>
-              <input id="input_value" type="text" placeholder="Product number"/>
+              <Form.Input id="input_value" type="text" placeholder="Product number"/>
             </Form.Field>
-            <Form.Field>
-              <label>Discount:</label>
-              <input type = "number" id="discount" value="10" onChange={this.changeDiscount}/>
-            </Form.Field>
+
+            </Form.Group>
+
             <Button type='submit' onClick={this.updateSearch}>Search</Button>
 
           </Form>
             <div>
             </div>
-          </Grid.Column>
-          <Grid.Column>
-          <div id="printarea">
+            </Grid.Column></Grid.Row>
+            <Grid.Row><Grid.Column>
+            <Divider />
           <Query
             query={GET_INFO_PRODUCT}
             skip={!this.state.partNumber}
-            variables={{partNumber: this.state.partNumber}} >
+            variables={{partNumber: this.state.partNumber, lang: this.state.lang}} >
 
           {({ loading, error, data }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error :</p>;
             if (data.product === null) return <p>Null product for reference number</p>;
-            this.printDocument(data);
             return (
-              <div id="panel">
 
-              <p><b>NAME:</b>{data.product.name}</p>
-              <p><b>TYPE: </b>{data.product.type}</p>
-              <p><b>NORMAL_PRICE: </b>{data.product.normalPrice}</p>
-              <p><b>SECOND_PRICE: </b>{data.product.normalPrice - (data.product.normalPrice * (this.state.discount/100))}</p>
-              <p><b>FAMILY_START_DATE: </b>{data.product.familyPrice_startDate}</p>
-              <p><b>FAMILY_END_DATE: </b>{data.product.familyPrice_endDate}</p>
-              <p><b>FAMILY_DISCLAIMER: </b>{data.product.familyPrice_disclaimer}</p>
-              </div>
+              <PtagPrinter
+                product={data.product}
+                />
+
             );
           }}
 
           </Query>
-          </div>
 
-          </Grid.Column>
-          <Grid.Column>
-          <Form>
-          <Form.Field control={Select} label='Size' name='size' options={DIM_OPTIONS} placeholder='Size' onChange={this.handleChange} />
-          <Button type='submit' onClick={this.printDocument}>Download PDF</Button>
-          </Form>
-
-          </Grid.Column>
-          </Grid.Row>
-
+          </Grid.Column></Grid.Row>
         </Grid>
         </Grid.Column></Grid.Row>
         </Grid>
--- src/templates/ptags.js
+++ src/templates/ptags.js
@@ -3,6 +3,50 @@ import * as images from './images';
 import * as jspdf_customfont from './fonts';
 
 
+const translations = {
+  de:{
+    fromDate: 'Gultig von ',
+    to: ' bis ',
+    header1: {
+      text:'Jetz',
+      size:'120'
+    },
+    header2: {
+      text:'im Angebot',
+      size:'78'
+    },
+    normalPrice: 'Normalpreis: '
+  },
+
+  fr:{
+    fromDate: 'Gultig von ',
+    to: ' bis ',
+    header1: {
+      text:'Maintenant',
+      size:'89'
+    },
+    header2: {
+      text:"sur l'offre",
+      size:'69'
+    },
+    normalPrice: 'Normalpreis: '
+  },
+  it:{
+    fromDate: 'Valido dal ',
+    to: ' al ',
+    header1: {
+      text:'Ora',
+      size:'155'
+    },
+    header2: {
+      text:"in Offerta",
+      size:'100'
+    },
+    normalPrice: 'Normalpreis: '
+  }
+}
+
+
 const ptag = {
 
   a5 : (product, discount) => {
@@ -37,7 +81,7 @@ const ptag = {
 
       doc.setFontSize(8);
       doc.setFontStyle('normal');
-      doc.text('Normale preis:',x,164);
+      doc.text(translations[product.lang].normalPrice,x,164);
 
       doc.setFontSize(30);
       doc.setFontStyle('bold');
@@ -49,12 +93,75 @@ const ptag = {
     doc.save(nameFile);
   },
 
-  pb25x60: (product, discount) => {
-    var doc = new jsPDF('p','mm',[250,600]);
+  pb25x60: (product, discount, start, end) => {
+    var doc = new jsPDF('p','mm',[500,600]);
     doc.addFont('Verdana.ttf', 'Verdana', 'normal','WinAnsiEncoding');
     doc.addFont('Verdana Bold.ttf', 'Verdana', 'bold','WinAnsiEncoding');
     doc.addFont('Verdana Bold Italic.ttf', 'Verdana', 'bold italic','WinAnsiEncoding');
     doc.addFont('Verdana Italic.ttf', 'Verdana', 'italic','WinAnsiEncoding');
+    const positions = [30, 280];
+    for (var i = 0; i < positions.length; i++) {
+      var x = positions[i];
+
+      doc.setFontSize(translations[product.lang].header1.size);
+      doc.setFontStyle('bold');
+      doc.text(translations[product.lang].header1.text, x, 67 );
+
+      doc.setFontSize(translations[product.lang].header2.size);
+      doc.text(translations[product.lang].header2.text, x, 100);
+
+      doc.setFontSize(42);
+      doc.setFontStyle('bold');
+      var t = doc.splitTextToSize(product.type,185);
+      if (t.length > 1) {
+        doc.text(t , x, 144 );
+      }else{
+        doc.text(t , x, 161 );
+      }
+
+      doc.setFontSize(72);
+      doc.setFontStyle('bold');
+      doc.text(product.name.toUpperCase(), x, 194 );
+
+      doc.setFontSize(30);
+      doc.setFontStyle('normal');
+      var t = doc.splitTextToSize('Characteristics dsf sd fsd fsd fsd ffsdf ',185);
+      doc.text(t, x, 212);
+
+      doc.setFontStyle('bold');
+      var priceDiscount= discount.toString();
+      var splitText = priceDiscount.split('.')
+      var size = doc.getStringUnitWidth(splitText[0] + '.'); // get the size
+      var fS = 280;
+      var width = size * fS; //widht of the box, 535pt -> for change to mm divide by (72/25.6)
+      const box = 310;
+
+      while (width >= box) {
+        fS= fS -10;
+        width = size * fS;
+      }
+
+      doc.setFontSize(fS);
+      doc.text(splitText[0] + '.', x, 460);
+      var height = doc.getTextDimensions('0');
+      doc.setFontSize(fS/2);
+      var h2 = doc.getTextDimensions('0');
+      var pos = 460 - h2.h;
+      doc.text(splitText[1], x+110, 460);
+
+
+      doc.setFontSize(32);
+      doc.setFontStyle('normal');
+      doc.text(translations[product.lang].fromDate + start + translations[product.lang].to + end, x, 500);
+
+      doc.setFontSize(50);
+      doc.setFontStyle('bold');
+      doc.text(translations[product.lang].normalPrice + product.normalPrice, x, 538);
+
+      doc.addImage(images.images.logo, 'JPEG', x+70, 558, 47,16);
+
+    }
+
     var nameFile = product.partNumber + '.pdf';
     doc.save(nameFile);
   },
